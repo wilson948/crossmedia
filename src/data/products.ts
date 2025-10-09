c6ed7c2096c649c4e7601d6a831ff6235ae6406e
franzraul666-del <franzraul666@gmail.com>
2025-10-09 01:54:39 -0400

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -532,7 +532,7 @@ image:"https://tse1.mm.bing.net/th/id/OIP.q_nZp-rRxUfnj1GxZmNKwQHaHa?rs=1&pid=Im
     id: 43,
     name: "Café Orgánico en Grano",
     price: 90.42,
-    image: "https://images.pexels.com/photos/416528/pexels-photo-416528.jpeg?auto=compress&cs=tinysrgb&w=600",
+    image: "hhttps://tse1.mm.bing.net/th/id/OIP.xmIxGU4LQSVIlyEKcopBawHaHa?rs=1&pid=ImgDetMain&o=7&rm=3",
     category: "beverages",
     brand: "MountainRoast",
     description: "Café orgánico en grano tostado medio",
