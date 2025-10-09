742f7c5e8f4efed500f2fa5709f65d32b8d0f27f
franzraul666-del <franzraul666@gmail.com>
2025-10-09 01:14:03 -0400

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -372,6 +372,7 @@ image:"https://tse1.mm.bing.net/th/id/OIP.q_nZp-rRxUfnj1GxZmNKwQHaHa?rs=1&pid=Im
     inStock: true,
     rating: 4.6
   },
+  
   {
     id: 27,
     name: "Queso Cheddar Añejo",
